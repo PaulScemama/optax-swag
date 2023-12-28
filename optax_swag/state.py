7fe3a27d2b0ce8a77f562136dae9a35fa100208c
Paul Scemama <pscemama@mitre.org>
2023-12-28 11:34:13 -0500

    Convert step to count in State

--- optax_swag/state.py
+++ optax_swag/state.py
@@ -5,25 +5,28 @@ import chex
 
 
 class SWAState(NamedTuple):
-  """State for SWAG mean and non-centered variance."""
-  mean: optax.Params  # Running mean of iterates.
-  step: chex.Array = jnp.zeros([], jnp.int32)  # Step count.
-  n: chex.Array = jnp.zeros([], jnp.int32)  # Iterate count using for running stats.
+    """State for SWAG mean and non-centered variance."""
+
+    mean: optax.Params  # Running mean of iterates.
+    count: chex.Array = jnp.zeros([], jnp.int32)  # Step count.
+    n: chex.Array = jnp.zeros([], jnp.int32)  # Iterate count using for running stats.
 
 
 class SWAGDiagState(NamedTuple):
-  """State for SWAG mean and diagonal non-centered variance."""
-  mean: optax.Params  # Running mean of iterates.
-  params2: optax.Params  # Running non-centered variance of iterates.
-  step: chex.Array = jnp.zeros([], jnp.int32)  # Step count.
-  n: chex.Array = jnp.zeros([], jnp.int32)  # Iterate count using for running stats.
+    """State for SWAG mean and diagonal non-centered variance."""
+
+    mean: optax.Params  # Running mean of iterates.
+    params2: optax.Params  # Running non-centered variance of iterates.
+    count: chex.Array = jnp.zeros([], jnp.int32)  # Step count.
+    n: chex.Array = jnp.zeros([], jnp.int32)  # Iterate count using for running stats.
 
 
 class SWAGState(NamedTuple):
-  """State for SWAG mean, diagonal non-centered variance and low rank terms."""
-  mean: optax.Params  # Running mean of iterates.
-  params2: optax.Params  # Running non-centered variance of iterates.
-  dparams: optax.Params  # Low rank delta columns.
-  step: chex.Array = jnp.zeros([], jnp.int32)  # Step count.
-  n: chex.Array = jnp.zeros([], jnp.int32)  # Iterate count using for running stats.
-  c: chex.Array = jnp.zeros([], jnp.int32)  # Current column to update.
+    """State for SWAG mean, diagonal non-centered variance and low rank terms."""
+
+    mean: optax.Params  # Running mean of iterates.
+    params2: optax.Params  # Running non-centered variance of iterates.
+    dparams: optax.Params  # Low rank delta columns.
+    count: chex.Array = jnp.zeros([], jnp.int32)  # Step count.
+    n: chex.Array = jnp.zeros([], jnp.int32)  # Iterate count using for running stats.
+    c: chex.Array = jnp.zeros([], jnp.int32)  # Current column to update.
